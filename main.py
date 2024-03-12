f84d5c7d413c27df4d95c1eab2443ef5d12f0668
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-12 23:52:57 +0000

    working model v.1

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 # define number checks, return numbers
-def checkInputNum (testNumeric):
+def checkInputNum(testNumeric):
   if testNumeric.isnumeric():
     return int(testNumeric)
   else:
     print(f"{testNumeric} is not a valid input. Please try again.")
     exit()
 
-# define function for range calculation 
-def computeRange(mathFunction):
-  return validNumShift // validTransaction // validDollarValue
+# define nested if for total bonus amount #
+def bonusCalculation(bonusRange):
+  if bonusRange <=30:
+    return bonus1
+  elif bonusRange > 30 and bonusRange <= 69:
+    return bonus2
+  elif bonusRange >= 70 and bonusRange <= 199:
+    return bonus3
+  else:
+    return bonus4
 
+
+
+
+bonus1 = 50.00
+bonus2 = 75.00
+bonus3 = 100.00
+bonus4 = 200.00
 
 #input
 employeeName = input("Please enter employee Name: ")
@@ -20,7 +34,10 @@ baseNumDollarValue = input("Dollar value of transactions: ")
 validNumShift = checkInputNum(baseNumShift)
 validTransaction = checkInputNum(baseNumTransaction)
 validDollarValue= checkInputNum(baseNumDollarValue)
-totalRange = computeRange()
+totalRange = (validDollarValue /validTransaction) /validNumShift
+
+finalBonus = bonusCalculation(totalRange)
 
 
 print(totalRange)
+print(finalBonus)
