c7969be2b64e89e4272584f2973fc7e2e6cb1056
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-13 00:20:20 +0000

    Completed assignment, tests passed

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 ## Program written to calculate empolyee bonuses based on: ##
 ## sales, dollar value, number of shift ##
 
+
+###### Housekeeping () ######
 ## Bonus Amount, fixed variables ##
 BONUS1 = 50.00
 BONUS2 = 75.00
 BONUS3 = 100.00
 BONUS4 = 200.00
 
-# define number checks, return numbers
+## define number checks, return valid integers for processing ##
 def checkInputNum(testNumeric):
   if testNumeric.isnumeric():
     return int(testNumeric)
@@ -15,7 +17,7 @@ def checkInputNum(testNumeric):
     print(f"{testNumeric} is not a valid input. Please try again.")
     exit()
 
-# define nested if for total bonus amount #
+## define nested if ranges for total bonus amount ##
 def bonusCalculation(bonusRange):
   if bonusRange <=30:
     return BONUS1
@@ -27,24 +29,22 @@ def bonusCalculation(bonusRange):
     return BONUS4
 
 
-#input
+###### Detail () ######
 employeeName = input("Please enter employee name: ")
-
 baseNumShift = input("Enter number of shifts: ")
 validNumShift = checkInputNum(baseNumShift)
-
 baseNumTransaction = input("Enter number of transactions: ")
 validTransaction = checkInputNum(baseNumTransaction)
-
 baseNumDollarValue = input("Enter transaction dollar value: ")
 validDollarValue= checkInputNum(baseNumDollarValue)
 
-
+# Compute total range to be processed #
 totalRange = (validDollarValue /validTransaction) /validNumShift
 
 
-## Outputs ##
+##### End-of-Job outputs () #####
 finalBonus = bonusCalculation(totalRange)
 print("-----")
 print(f"Employee Name: {employeeName}")
-print(f"Employee Bonus: {finalBonus}")
+print("Employee Bonus: ${:.2f}".format(finalBonus))
+
